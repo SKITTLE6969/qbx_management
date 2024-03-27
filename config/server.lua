d911d532a8802a91cf29e5c3d122f8b1796f34e1
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2024-03-27 15:16:19 +0530

    Update server.lua

--- config/server.lua
+++ config/server.lua
@@ -12,17 +12,53 @@ return {
 
     ---@type table<GroupName, ZoneInfo>
     menus = {
-        lostmc = {
-            coords = vec3(983.69, -90.92, 74.85),
+        ambulance = {
+            coords = vec3(328.61, -602.31, 43.27),
             size = vec3(1.5, 1.5, 1.5),
             rotation = 39.68,
-            type = 'gang',
+            type = 'job',
         },
-        vagos = {
-            coords = vec3(351.18, -2054.92, 22.09),
+        police = {
+            coords = vec3(-305.09, -1045.77, 31.25),
             size = vec3(1.5, 1.5, 1.5),
             rotation = 39.68,
-            type = 'gang',
+            type = 'job',
+        },
+        taxi = {
+            coords = vec3(887.02, -163.03, 78.41),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
+        },
+        mechanic = {
+            coords = vec3(86.09, 6530.0, 34.87),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
+        },
+        uwu = {
+            coords = vec3(-596.92, -1053.37, 22.34),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
+        },
+        burgershot = {
+            coords = vec3(-1199.0, -903.8, 13.89),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
+        },
+        whitewidow = {
+            coords = vec3(376.25, -823.84, 29.3),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
+        },
+        tequilala = {
+            coords = vec3(-565.65, 285.12, 85.38),
+            size = vec3(1.5, 1.5, 1.5),
+            rotation = 39.68,
+            type = 'job',
         },
     },
 }
